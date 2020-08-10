fd99a56cbc1113e2232f176eca6a12a6f6045d59
crclz <crucialize@foxmail.com>
2020-08-10 19:52:26 +0800

    Add string length limit for CreateDocumentModel and PatchDocumentModel

--- src/main/java/fullforum/dto/in/CreateDocumentModel.java
+++ src/main/java/fullforum/dto/in/CreateDocumentModel.java
@@ -1,7 +1,14 @@
 package fullforum.dto.in;
 
+import javax.validation.constraints.Size;
+
 public class CreateDocumentModel {
+
+    @Size(min = 1, max = 25)
     public String title;
+
+    @Size(max = 140)
     public String description;
+
     public String data;
 }
