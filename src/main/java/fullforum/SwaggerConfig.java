2f3233d43f76009f535cfd7fef0d313b00c05be6
lhs1116 <920741917@qq.com>
2020-08-11 17:10:12 +0800

    Pass CommentController Test

--- src/main/java/fullforum/SwaggerConfig.java
+++ src/main/java/fullforum/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package fullforum;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
