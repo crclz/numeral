569864a24a23586f9c8c6e79651dca4d159fc9ac
crclz <crucialize@foxmail.com>
2020-08-16 22:16:27 +0800

    Fix mvn package error of outputing too much

--- thin-ci.sh
+++ thin-ci.sh
@@ -8,7 +8,7 @@ sleep 2
 echo "checking wait-for-it.sh integrity"
 echo "58483b01170dc5cdfb602f5a6da7f6ee49d4482619a6018983207780dc27bdbc *wait-for-it.sh" | sha256sum -c -
 
-mvn package
+mvn package > mvn-package-output.txt
 
 docker build -f thin.Dockerfile . -t registry.cn-hangzhou.aliyuncs.com/crucialize/numeral:latest
 
